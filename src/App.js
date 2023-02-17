79a0c870317e17592290569efa0ec6f8ae3b7c23
Nathalia Azevedo <nathaliaazmelo@gmail.com>
2023-02-17 17:38:58 -0300

    Added basic header structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import myImage1 from './images/image-one.svg';
+import myImage2 from './images/image-two.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="tertiary">Find your block</div>
+        <h1>
+          Encontre os melhores{" "}
+          <div className="secondary">blocos de carnaval</div> de 2023
+        </h1>
+        <div className="input-wrapper">
+          <input type="search" id="site-search" name="q" placeholder="Pesquise por nome"/>
+          <select name="cities" id="cities" placeholder="Selecione sua cidade">
+            <option value="" disabled selected hidden>Selecione uma cidade</option>
+            <option value="sp">São Paulo</option>
+            <option value="rj">Rio de Janeiro</option>
+          </select>
+          <button>Buscar agora</button>
+          <img className="image-one" src={myImage1}/>
+          <img className="image-two" src={myImage2}/>
+        </div>
       </header>
     </div>
   );
