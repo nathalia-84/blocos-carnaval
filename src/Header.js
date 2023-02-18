baf15ad91be00ba5196886fdc41623f386e4624d
Nathalia Azevedo <nathaliaazmelo@gmail.com>
2023-02-18 13:24:06 -0300

    Added styles to header structure

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,17 @@
 import myImage1 from './images/image-one.svg';
 import myImage2 from './images/image-two.svg';
-import "./App.css";
+import "./Header.css";
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
+      <header className="Header">
         <div className="tertiary">Find your block</div>
         <h1>
-          Encontre os melhores{" "}
-          <div className="secondary">blocos de carnaval</div> de 2023
+          Encontre os <span className="secondary">melhores blocos</span> de carnaval de 2023
         </h1>
         <div className="input-wrapper">
-          <input type="search" id="site-search" name="q" placeholder="Pesquise por nome"/>
-          <select name="cities" id="cities" placeholder="Selecione sua cidade">
+          <input className='input-one' type="search" id="site-search" name="q" placeholder="Pesquise por nome"/>
+          <select className='select' name="cities" id="cities" placeholder="Selecione sua cidade">
             <option value="" disabled selected hidden>Selecione uma cidade</option>
             <option value="sp">São Paulo</option>
             <option value="rj">Rio de Janeiro</option>
@@ -23,8 +21,7 @@ function App() {
           <img className="image-two" src={myImage2}/>
         </div>
       </header>
-    </div>
   );
 }
 
-export default App;
+export default Header;
